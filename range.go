add65cb11c5d3faadf50a7d32e3b187a89bd320c
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-08 18:39:09 +0200

    fix runtime version of 'in'

--- range.go
+++ range.go
@@ -52,11 +52,35 @@ func In[T comparable](val T, slice []T) bool {
 }
 
 // InRuntime is a runtime analogue of In (made to be compatible with templates).
-func InRuntime(val any, slice []any) bool {
-	for _, v := range slice {
-		if v == val {
-			return true
-		}
+func InRuntime(val any, slice any) bool {
+	switch val := val.(type) {
+	case int:
+		return In(val, slice.([]int))
+	case int8:
+		return In(val, slice.([]int8))
+	case int16:
+		return In(val, slice.([]int16))
+	case int32:
+		return In(val, slice.([]int32))
+	case int64:
+		return In(val, slice.([]int64))
+	case uint:
+		return In(val, slice.([]uint))
+	case uint8:
+		return In(val, slice.([]uint8))
+	case uint16:
+		return In(val, slice.([]uint16))
+	case uint32:
+		return In(val, slice.([]uint32))
+	case uint64:
+		return In(val, slice.([]uint64))
+	case float32:
+		return In(val, slice.([]float32))
+	case float64:
+		return In(val, slice.([]float64))
+	case string:
+		return In(val, slice.([]string))
+	default:
+		panic("unknown type for InRuntime()")
 	}
-	return false
 }

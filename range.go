41ebe786d1eab611aeb580254729b7be439614e3
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-08 11:40:56 +0200

    filter any map functions

--- range.go
+++ range.go
@@ -1,5 +1,23 @@
 package zen
 
+func Filter[T any](slice []T, fn func(any) bool) []T {
+	var a []T
+	for _, v := range slice {
+		if fn(v) {
+			a = append(a, v)
+		}
+	}
+	return a
+}
+
+func Map[T1 any, T2 any](slice []T1, fn func(T1) T2) []T2 {
+	a := make([]T2, len(slice))
+	for i, v := range slice {
+		a[i] = fn(v)
+	}
+	return a
+}
+
 func Range(from, to int) []int {
 	a := make([]int, to-from+1)
 	for i := 0; i <= to-from; i++ {

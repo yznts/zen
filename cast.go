933a25a54210f5ddaf53dc75454245e68ae7d648
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-11 13:52:58 +0200

    CastPSlice function

--- cast.go
+++ cast.go
@@ -12,3 +12,21 @@ func CastSlice[T any](slice []any) []T {
 	}
 	return _slice
 }
+
+// CastPSlice is a function to cast a slice of any values ([]any)
+// to a slice of the given type pointers.
+//
+// Usage:
+//  zen.CastPSlice(int)([]any{1, 2, nil}) []*int{1, 2, nil}
+func CastPSlice[T any](slice []any) []*T {
+	_slice := make([]*T, len(slice))
+	for _, v := range slice {
+		if v == nil {
+			_slice = append(_slice, nil)
+		} else {
+			_v := v.(T)
+			_slice = append(_slice, &_v)
+		}
+	}
+	return _slice
+}

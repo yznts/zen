4c98cb06f6edebfd656a55c7c23d421bea6a1505
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-11 11:35:59 +0200

    Must func doc

--- must.go
+++ must.go
@@ -1,5 +1,7 @@
 package zen
 
+// Must is a helper that wraps a call to a function returning
+// and panics if the error is non-nil.
 func Must[T any](val T, err error) T {
 	if err != nil {
 		panic(err)

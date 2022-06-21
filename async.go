0877a89516c8d0337e2952adce1685907a68ae8a
Yurii Zinets <yurii.zinets@icloud.com>
2022-06-21 15:26:16 +0100

    support json (un)marshalling for future

--- async.go
+++ async.go
@@ -30,12 +30,28 @@
 */
 package zen
 
+import "encoding/json"
+
 // Future is an awaitable object.
 // Behavior is similar to JS Promise.
 type Future[T any] struct {
 	value chan T
 	cache *T
 	err   error
+}
+
+// MarshalJSON implements future marshalling.
+func (f *Future[T]) MarshalJSON() ([]byte, error) {
+	val, err := Await(f)
+	if err != nil {
+		return []byte{}, err
+	}
+	return json.Marshal(val)
+}
+
+// MarshalJSON implements future unmarshalling.
+func (c *Future[T]) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &c.cache)
 }
 
 // Await for a future object.

ed2881fa2e74ffc45cf04ec9ea45548fdd44c5e0
Yurii Zinets <yurii.zinets@icloud.com>
2022-09-20 13:46:41 +0200

    response functions refactor

--- response.go
+++ response.go
@@ -24,76 +24,125 @@ package zen
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 )
 
-// ResponseWrapper is a wrapper for http.Response.
-// It provides a few useful extra methods.
+// ResponseWrapper is a wrapper around http.Response.
+// It provides a set of functions for a chained response processing.
 type ResponseWrapper struct {
 	*http.Response
+
+	err error
 }
 
-// Must ensures that response code is between 200 and 299.
+// Must is a chain closer.
+// Ensures that there was no errors in processing chain.
 // If not, it panics.
-// Returns itself for chaining.
-func (r *ResponseWrapper) Must() *ResponseWrapper {
-	if r.StatusCode < 200 || r.StatusCode > 299 {
-		panic("unexpected response code")
+func (r *ResponseWrapper) Must() {
+	if r.err != nil {
+		panic(r.err)
 	}
-	return r
+}
+
+// Ensure is a chain closer.
+// Ensures that there was no errors in processing chain.
+// If not, returns an error.
+func (r *ResponseWrapper) Ensure() error {
+	return r.err
 }
 
 // Debug prints the response to stdout.
-// Returns itself for chaining.
+// If something goes wrong during dump, chain execution will be stopped.
+// Returns wrapper for chaining.
 func (r *ResponseWrapper) Debug() *ResponseWrapper {
-	dump, err := httputil.DumpResponse(r.Response, true)
-	if err != nil {
-		panic(err)
+	// Check error status
+	if r.err != nil {
+		return r
 	}
+	// Dump raw response
+	dump, err := httputil.DumpResponse(r.Response, true)
+	// If we got an error, prevent further chain execution
+	if err != nil {
+		r.err = err
+		return r
+	}
+	// Print raw response
 	println(string(dump))
+	// Return wrapper
 	return r
 }
 
-// Map detects response type and decodes it to map[string]any{}.
-// If the response type is not supported, it panics.
-func (r *ResponseWrapper) Map() map[string]any {
-	data := map[string]any{}
-	if r.Header.Get("Content-Type") == "application/json" {
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			panic(err)
-		}
-	} else {
-		panic("unsupported response type")
+// Success ensures that response code is between 200 and 299.
+// If not, chain execution will be stopped.
+// Returns wrapper for chaining.
+func (r *ResponseWrapper) Success() *ResponseWrapper {
+	// Check error status
+	if r.err != nil {
+		return r
 	}
-	return data
+	// Check status code
+	if r.StatusCode < 200 || r.StatusCode > 299 {
+		// Prevent further chain execution
+		r.err = fmt.Errorf("status code is not between 200 and 299: %d", r.StatusCode)
+	}
+	// Return wrapper
+	return r
 }
 
-// Text reads response body as a text.
-// If something wrong, it panics.
-func (r *ResponseWrapper) Text() string {
+// Text reads response body as a text into provided target.
+// If something goes wrong during reading, chain execution will be stopped.
+// Returns wrapper for chaining.
+func (r *ResponseWrapper) Text(target *string) *ResponseWrapper {
+	// Check error status
+	if r.err != nil {
+		return r
+	}
+	// Read response body
 	bts, err := ioutil.ReadAll(r.Body)
+	// If we got an error, prevent further chain execution
 	if err != nil {
-		panic(err)
+		r.err = err
+		return r
 	}
-	return string(bts)
+	// Set value to the target
+	*target = string(bts)
+	// Return wrapper
+	return r
 }
 
-// Decode detects response type and decodes it to target.
-// If the response type is not supported, it panics.
-func (r *ResponseWrapper) Decode(target any) {
-	if r.Header.Get("Content-Type") == "application/json" {
-		if err := json.NewDecoder(r.Body).Decode(&target); err != nil {
-			panic(err)
-		}
-	} else {
-		panic("unsupported response type")
+// Decode detects response type and decodes it into target.
+// If response type is not supported, or there is an error during decoding,
+// chain execution will be stopped.
+// Returns wrapper for chaining.
+func (r *ResponseWrapper) Decode(target any) *ResponseWrapper {
+	// Check error status
+	if r.err != nil {
+		return r
 	}
+	// Process response
+	if r.Header.Get("Content-Type") == "application/json" { // JSON
+		// If we got an error, prevent further chain execution
+		if err := json.NewDecoder(r.Body).Decode(&target); err != nil {
+			r.err = err
+			return r
+		}
+	} else { // If not detected, prevent further chain execution
+		r.err = fmt.Errorf("content type is not detected by library: %s", r.Header.Get("Content-Type"))
+		return r
+	}
+	// Return wrapper
+	return r
 }
 
 // Response wraps *http.Response with own wrapper,
-// providing extra methods.
-func Response(resp *http.Response) *ResponseWrapper {
-	return &ResponseWrapper{resp}
+// providing extra functions.
+// See `ResponseWrapper` for details.
+func Response(resp *http.Response, err ...error) *ResponseWrapper {
+	if len(err) == 0 {
+		err = append(err, nil)
+	}
+	return &ResponseWrapper{resp, err[0]}
 }

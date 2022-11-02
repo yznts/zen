80c07677658f55deb8adf1466f702a3e4bfc10ef
Yurii Zinets <yurii.zinets@icloud.com>
2022-11-02 13:21:58 +0100

    unique function

--- range.go
+++ range.go
@@ -28,6 +28,23 @@ func Map[T1 any, T2 any](slice []T1, fn func(v T1) T2) []T2 {
 	return a
 }
 
+// Unique returns a new slice with the unique slice values.
+// Comparable value is defined by a given function.
+//
+// Usage:
+//
+//	Unique([]int{1, 2, 2, 3}, func(v int) int { return v }) // []int{1, 2, 3}
+func Unique[T1 any, T2 comparable](slice []T1, fn func(v T1) T2) []T1 {
+	flags := map[T2]bool{}
+	return Filter(slice, func(v T1) bool {
+		compareval := fn(v)
+		defer func() {
+			flags[compareval] = true
+		}()
+		return !flags[compareval]
+	})
+}
+
 // Range returns a new slice of integers in the given range (from, to).
 //
 // Usage:

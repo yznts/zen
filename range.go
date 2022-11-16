16108338717b0ad4aa43fe21d939e46457e4484d
Yurii Zinets <yurii.zinets@icloud.com>
2022-11-16 11:24:15 +0100

    index function

--- range.go
+++ range.go
@@ -1,5 +1,20 @@
 package zen
 
+// Index finds element index according to the given function.
+// If nothing found, returns -1.
+//
+// Usage:
+//
+//	Index([]int{1, 2, 3}, func(v int) bool { return v > 2 }) // 2
+func Index[T any](slice []T, fn func(v T) bool) int {
+	for i, v := range slice {
+		if fn(v) {
+			return i
+		}
+	}
+	return -1
+}
+
 // Filter returns filtered slice according to the given function.
 //
 // Usage:

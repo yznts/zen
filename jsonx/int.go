f043b86d27240b920f0b38efee1909618e0561b4
Yurii Zinets <yurii.zinets@icloud.com>
2023-07-14 16:43:36 +0200

    jsonx.Int null case

--- jsonx/int.go
+++ jsonx/int.go
@@ -7,8 +7,15 @@ import "strconv"
 type Int int
 
 func (i *Int) UnmarshalJSON(b []byte) error {
+	// Handle null case.
+	if string(b) == "null" {
+		// Set default value
+		*i = 0
+		// Return nil
+		return nil
+	}
 	// Parse string as float.
-	// This approach will cover both cases.
+	// This approach will cover both int and float cases.
 	v, err := strconv.ParseFloat(string(b), 64)
 	// Set the value
 	*i = Int(v)

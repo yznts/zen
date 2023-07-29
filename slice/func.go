048287103d773a6512b1dc3475b68d0620e0b137
Yurii Zinets <yurii.zinets@icloud.com>
2023-07-29 15:15:46 +0200

    slice/func refactor

--- slice/func.go
+++ slice/func.go
@@ -2,15 +2,15 @@ package slice
 
 //nolint:dupword
 /*
-FuncIn returns a function that checks if the given value is in the given top-level values slice.
+InFunc returns a function that checks if the given value is in the given top-level values slice.
 Works only for comparable types.
 Useful for filtering slices with slice.Filter.
 
 Usage:
 
-	slice.Filter([]int{1, 2, 3, 3, 4, 5}, slice.FilterIn(1, 2, 3)) // []int{1, 2, 3, 3}
+	slice.Filter([]int{1, 2, 3, 3, 4, 5}, slice.InFunc(1, 2, 3)) // []int{1, 2, 3, 3}
 */
-func FuncIn[T comparable](values ...T) func(T) bool {
+func InFunc[T comparable](values ...T) func(T) bool {
 	return func(t T) bool {
 		return In(t, values)
 	}
@@ -18,15 +18,15 @@ func FuncIn[T comparable](values ...T) func(T) bool {
 
 //nolint:dupword
 /*
-FuncNotIn returns a function that checks if the given value is not in the given top-level values slice.
+NotInFunc returns a function that checks if the given value is not in the given top-level values slice.
 Works only for comparable types.
 Useful for filtering slices with slice.Filter.
 
 Usage:
 
-	slice.Filter([]int{1, 2, 3, 3, 4, 5}, slice.FilterNotIn(1, 2, 3)) // []int{4, 5}
+	slice.Filter([]int{1, 2, 3, 3, 4, 5}, slice.NotInFunc(1, 2, 3)) // []int{4, 5}
 */
-func FuncNotIn[T comparable](values ...T) func(T) bool {
+func NotInFunc[T comparable](values ...T) func(T) bool {
 	return func(t T) bool {
 		return !In(t, values)
 	}

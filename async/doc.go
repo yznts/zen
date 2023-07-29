fade91fb71a932e2da1dca170a4cbb9810992b68
Yurii Zinets <yurii.zinets@icloud.com>
2023-07-29 15:20:10 +0200

    async/doc update

--- async/doc.go
+++ async/doc.go
@@ -2,6 +2,6 @@
 async - a package that provides tools for asynchronous execution using built-in goroutines.
 Provides API which is very similar to JavaScript Promises.
 On top of existing async/await functionality,
-package also provides high-level functions like .Filter() and .Map().
+package also provides high-level functions like .Map().
 */
 package async
